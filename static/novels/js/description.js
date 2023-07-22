32cc1cd19ed7693b98b2806129f4080d1e7bd153
immodi <ay727467@gmail.com>
2023-07-22 15:05:35 +0300

    NaN (update)

--- static/novels/js/description.js
+++ static/novels/js/description.js
@@ -3,7 +3,7 @@ const currentPageNumber = parseInt(currentUrl.slice(currentUrl.length - 1, curre
 
 let currentPageNumberInList = Array.from(document.querySelector(".novels-total").children)
 
-if (currentPageNumber === 1 || currentPageNumber == NaN) {
+if (currentPageNumber === 1 || isNaN(currentPageNumber)) {
     currentPageNumberInList = currentPageNumberInList[0]
 } else {
     currentPageNumberInList = currentPageNumberInList[1]
