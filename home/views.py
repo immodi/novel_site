0c0516c9460a5d680cf1185fef338d42b4dfaf62
immodi <ay727467@gmail.com>
2023-10-03 18:48:11 +0300

    noimage / opt

--- home/views.py
+++ home/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from novels.models import NovelModel
-import random
+from random import randrange
 
-# Create your views here.
 class HomeView(TemplateView):
     template_name = "home\index.html"
     
     def get(self, request):
-        all_novels = NovelModel.objects.all()
-        hot_novels = random.sample(list(all_novels), 21)
+        random_start = randrange(31)
+        hot_novels = NovelModel.objects.all()[random_start:random_start+21]
         # hot_novels = all_novels.order_by("id")[(len(all_novels)-21):][::-1]
         
         # import pdb; pdb.set_trace()
